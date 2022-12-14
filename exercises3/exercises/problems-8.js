94e92b9491e3ec46f5889c9c5c0ddc9d0b25f8d0
Bryan Higgins <118735768+bryhigg@users.noreply.github.com>
2022-12-14 16:45:18 +0000

    Update

--- exercises3/exercises/problems-8.js
+++ exercises3/exercises/problems-8.js
@@ -9,27 +9,29 @@
 */
 
 function react(sequence) {
-    arr = []
-    arr1 = []
-    
-    for(i in sequence){
-        arr.push(i)
-    }
-
-    arr1.from(arr)
-
-    for(i = 0; i < arr.length; i++){
-        if(i!= arr.length-1){
-            if(arr1[i].toLowerCase() == arr1[i+1].toLowerCase()){
-                if((arr[i].toLowerCase() || arr[i+1].toLowerCase()) && !(arr[i].toLowerCase() && arr[i+1].toLowerCase())){
-    
-                }
-            }
+    // Keep track of the current sequence and the index of the last
+    // processed letter.
+    let currentSequence = sequence;
+    let lastProcessedIndex = -1;
+  
+    // Continue processing the sequence until no more letters react.
+    while (lastProcessedIndex < currentSequence.length - 1) {
+      // Check for any reacting letters.
+      for (let i = lastProcessedIndex + 1; i < currentSequence.length - 1; i++) {
+        const a = currentSequence[i];
+        const b = currentSequence[i + 1];
+  
+        // If the two letters react, remove them from the sequence.
+        if (a !== b && a.toLowerCase() === b.toLowerCase()) {
+          currentSequence = currentSequence.slice(0, i) + currentSequence.slice(i + 2);
+          lastProcessedIndex = i - 1;
+          break;
         }
-
+      }
     }
-}
-
+  
+    return currentSequence;
+  }
 // Do not modify this code
 window.solutions = window.solutions || {}
 window.solutions.react = react
